c99a3af1cbe37321acf16e907b748b7e6c98103f
Ashfin Ramadhandy E <ashfinramadhandy10@gmail.com>
2018-11-04 19:14:36 +0700

    commit

---
+++ src/Pembelian.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Dhandy
+ */
+public class Pembelian {
+    public SepedaMotor beliMotor;
+    public Pembeli pembeli;
+
+    public Pembelian(SepedaMotor beliMotor, Pembeli pembeli) {
+        this.beliMotor = beliMotor;
+        this.pembeli = pembeli;
+    }
+
+   
+   void dataPembelian(){
+       System.out.println("Data Pembelian Sepeda Motor");
+       System.out.println("Nama : "+pembeli.getNama());
+       System.out.println("Alamat : "+pembeli.getAlamat());
+       System.out.println("Beli Sepeda Motor : "+beliMotor.Merk);
+       System.out.println("Dengan Transmisi : "+beliMotor.jenis);
+   }
+}
