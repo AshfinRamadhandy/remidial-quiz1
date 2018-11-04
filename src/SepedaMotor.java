0482834dc222198b3bd5b50e045929187515e7bd
Dhandy <Dhandy@DESKTOP-VIG5JU5>
2018-11-04 18:38:12 +0700

    commit class sepeda motorr

---
+++ src/SepedaMotor.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Dhandy
+ */
+public class SepedaMotor {
+    public String nama;
+    public String jenis;
+
+    public SepedaMotor(String nama, String jenis) {
+        this.nama = nama;
+        this.jenis = jenis;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getJenis() {
+        return jenis;
+    }
+
+    public void setJenis(String jenis) {
+        this.jenis = jenis;
+    }
+    
+    
+}
